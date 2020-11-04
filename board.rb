155fae47022631f1e41426ca10364d808ec52b25
Morghan <morghan102@gmail.com>
2020-11-04 06:21:26 -0600

    fixed []=

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require "byebug"
 
 class Board
   def self.empty_grid
@@ -26,7 +27,8 @@ class Board
     grid[x][y]
   end
 
-  def []=(pos, value)
+  def []=(value, pos)
+    # debugger
     x, y = pos
     tile = grid[x][y]
     tile.value = value
